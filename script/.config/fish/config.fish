118702de6c952a8567bb8e36fdd6e73018598873
syanhai_yakisoba=\chan! <koumaza--@outlook.com>
2020-06-29 12:40:14 +0900

    Update config.fish

--- script/.config/fish/config.fish
+++ script/.config/fish/config.fish
@@ -1,7 +1,7 @@
 alias repo2sync="repo sync -c -j(nproc --all) --force-sync --no-clone-bundle --no-tags"
 set -gx PATH /opt/bin $PATH
 set -gx USE_CCACHE 1
-set -gx CCACHE_DIR ~/.ccache
+set -gx CCACHE_DIR ~/android/.ccache
 set -gx CC "ccache gcc"
 alias CC="ccache gcc"
 alias gcc="ccache gcc"
